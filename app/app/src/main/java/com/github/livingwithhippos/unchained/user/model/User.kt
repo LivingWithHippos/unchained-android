7b969bcf5ef13bd30905480a3ad47d05001b1f0c
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-03 14:51:40 +0200

    added User data class and api

---
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/user/model/User.kt
@@ -0,0 +1,35 @@
+package com.github.livingwithhippos.unchained.user.model
+
+import com.squareup.moshi.Json
+import retrofit2.Response
+import retrofit2.http.GET
+
+data class User(
+    @Json(name = "id")
+    val id: Int,
+    @Json(name = "username")
+    val username: String,
+    @Json(name = "email")
+    val email: String,
+    @Json(name = "points")
+    val points: Int,
+    @Json(name = "locale")
+    val locale: String,
+    @Json(name = "avatar")
+    val avatar: String,
+    @Json(name = "type")
+    val type: String,
+    @Json(name = "premium")
+    val premium: Int,
+    @Json(name = "expiration")
+    val expiration: String
+)
+
+data class UserResponse(
+    val result: User
+)
+
+interface UserApi {
+    @GET("user")
+    suspend fun getUser(): Response<User>
+}
