256a4942f3e4c06883d903b2074ae5c7ad2cc333
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-03 19:34:42 +0200

    added authentication data class and api

---
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/authentication/model/Authentication.kt
@@ -0,0 +1,33 @@
+package com.github.livingwithhippos.unchained.authentication.model
+
+import com.github.livingwithhippos.unchained.utilities.OPEN_SOURCE_CLIENT_ID
+import com.squareup.moshi.Json
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+data class Authentication(
+    @Json(name = "device_code")
+    val deviceCode: String,
+    @Json(name = "user_code")
+    val userCode: String,
+    @Json(name = "interval")
+    val interval: Int,
+    @Json(name = "expires_in")
+    val expiresIn: Int,
+    @Json(name = "verification_url")
+    val verificationUrl: String
+)
+
+data class AuthenticationResponse(
+    val result: Authentication
+)
+
+interface AuthenticationApi {
+
+    @GET("/device/code")
+    suspend fun getAuthentication(@Query("client_id") id: String = OPEN_SOURCE_CLIENT_ID,
+                                  @Query("new_credentials") newCredentials: String = "yes"
+    ): Response<Authentication>
+}
