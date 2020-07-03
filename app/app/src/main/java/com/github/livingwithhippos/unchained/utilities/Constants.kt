3628d64901fa41a8a8b99710ae1fa84ffcfae057
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-03 12:16:58 +0200

    added web constants

---
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/Constants.kt
@@ -0,0 +1,7 @@
+package com.github.livingwithhippos.unchained.utilities
+
+const val OPEN_SOURCE_CLIENT_ID = "X245A4XAIBGVM"
+
+const val BASE_URL = "https://api.real-debrid.com/rest/1.0/"
+
+const val BASE_AUTH_URL = "https://api.real-debrid.com/oauth/v2/device/code/"
