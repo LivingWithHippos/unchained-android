879a1feda028ab1400812ba334aea467778824e8
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-03 19:42:09 +0200

    Create AuthenticationRepository.kt

---
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/authentication/model/AuthenticationRepository.kt
@@ -0,0 +1,17 @@
+package com.github.livingwithhippos.unchained.authentication.model
+
+import com.github.livingwithhippos.unchained.base.model.BaseRepository
+
+class AuthenticationRepository (private val api: AuthenticationApi) : BaseRepository() {
+
+    suspend fun getVerificationCode(): Authentication? {
+
+        val authResponse = safeApiCall(
+            call = { api.getAuthentication() },
+            errorMessage = "Error Fetching Authentication Info"
+        )
+
+        return authResponse;
+
+    }
+}
