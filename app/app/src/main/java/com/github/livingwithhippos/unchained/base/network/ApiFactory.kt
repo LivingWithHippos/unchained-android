dc2083402fccd0056355ebff25d37a233e0d6615
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-03 14:53:50 +0200

    added api factory

---
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/base/network/ApiFactory.kt
@@ -0,0 +1,45 @@
+package com.github.livingwithhippos.unchained.base.network
+
+import com.github.livingwithhippos.unchained.user.model.UserApi
+import com.github.livingwithhippos.unchained.utilities.BASE_URL
+import com.github.livingwithhippos.unchained.utilities.OPEN_SOURCE_CLIENT_ID
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object ApiFactory {
+
+    //Creating Auth Interceptor to add api_key query in front of all the requests.
+    private val authInterceptor = Interceptor { chain ->
+        val newUrl = chain.request().url
+            .newBuilder()
+            .addQueryParameter("client_id", OPEN_SOURCE_CLIENT_ID)
+            .build()
+
+        val newRequest = chain.request()
+            .newBuilder()
+            .url(newUrl)
+            .build()
+
+        chain.proceed(newRequest)
+    }
+
+    //OkhttpClient for building http request url
+    private val debridClient = OkHttpClient().newBuilder()
+        .addInterceptor(authInterceptor)
+        .build()
+
+
+    fun retrofit(): Retrofit = Retrofit.Builder()
+        .client(debridClient)
+        .baseUrl(BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+
+
+    val userApi: UserApi = retrofit().create(
+        UserApi::class.java
+    )
+
+}
