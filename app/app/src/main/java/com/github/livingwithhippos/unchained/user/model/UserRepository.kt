7b187d711a019a0d2f3f3d5e35f87607328e7fd4
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-03 14:53:42 +0200

    added user repository

---
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/user/model/UserRepository.kt
@@ -0,0 +1,19 @@
+package com.github.livingwithhippos.unchained.user.model
+
+import com.github.livingwithhippos.unchained.base.model.BaseRepository
+
+
+class UserRepository(private val api: UserApi) : BaseRepository() {
+
+    suspend fun getUserInfo(): User? {
+
+        val userResponse = safeApiCall(
+            call = { api.getUser() },
+            errorMessage = "Error Fetching User Info"
+        )
+
+        return userResponse;
+
+    }
+
+}
