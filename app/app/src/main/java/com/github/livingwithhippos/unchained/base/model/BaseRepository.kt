4ba3f52513e18640fe1266a4c05fa1f76070e9ac
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-03 14:53:25 +0200

    added basic network classes
    NetworkReponse holds the erro/result response
    BaseRepository uses it for api calls

---
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/base/model/BaseRepository.kt
@@ -0,0 +1,37 @@
+package com.github.livingwithhippos.unchained.base.model
+
+import android.util.Log
+import com.github.livingwithhippos.unchained.base.network.NetworkResponse
+import retrofit2.Response
+import java.io.IOException
+
+open class BaseRepository {
+
+    suspend fun <T : Any> safeApiCall(call: suspend () -> Response<T>, errorMessage: String): T? {
+
+        val result: NetworkResponse<T> = safeApiResult(call, errorMessage)
+        var data: T? = null
+
+        when (result) {
+            is NetworkResponse.Success ->
+                data = result.data
+            is NetworkResponse.Error -> {
+                Log.d("BaseRepository", "$errorMessage - Exception : ${result.exception}")
+            }
+        }
+
+
+        return data
+
+    }
+
+    private suspend fun <T : Any> safeApiResult(
+        call: suspend () -> Response<T>,
+        errorMessage: String
+    ): NetworkResponse<T> {
+        val response = call.invoke()
+        if (response.isSuccessful) return NetworkResponse.Success(response.body()!!)
+
+        return NetworkResponse.Error(IOException("Error Occurred while getting api result, error : $errorMessage"))
+    }
+}
---
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/base/network/NetworkResponse.kt
@@ -0,0 +1,12 @@
+package com.github.livingwithhippos.unchained.base.network
+
+/**
+ * see https://kotlinlang.org/docs/reference/generics.html#declaration-site-variance for an explanation of the out keyword.
+ * T and U will be used as return types
+ */
+
+
+sealed class NetworkResponse<out T : Any> {
+    data class Success<out T : Any>(val data: T) : NetworkResponse<T>()
+    data class Error(val exception: Exception) : NetworkResponse<Nothing>()
+}
