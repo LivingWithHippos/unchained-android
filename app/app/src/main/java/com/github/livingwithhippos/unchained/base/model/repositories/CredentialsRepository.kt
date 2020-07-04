98f837e83179c4cbcaf9e7f30c49ea60196e30f5
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-04 17:02:39 +0200

    added credentials repository

---
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/base/model/repositories/CredentialsRepository.kt
@@ -0,0 +1,18 @@
+package com.github.livingwithhippos.unchained.base.model.repositories
+
+import com.github.livingwithhippos.unchained.base.model.dao.CredentialsDao
+import com.github.livingwithhippos.unchained.base.model.entities.Credentials
+
+// Declares the DAO as a private property in the constructor. Pass in the DAO
+// instead of the whole database, because you only need access to the DAO
+class CredentialsRepository(private val credentialsDao: CredentialsDao) {
+
+    suspend fun getAllCredentials() = credentialsDao.getAllCredentials()
+
+    suspend fun getCredentials(username: String) = credentialsDao.getCredentials(username)
+
+    suspend fun updateSecrets(username: String, clientId: String, clientSecret: String) =
+        credentialsDao.updateSecrets(username, clientId, clientSecret)
+
+    suspend fun insert(credentials: Credentials) = credentialsDao.insert(credentials)
+}
