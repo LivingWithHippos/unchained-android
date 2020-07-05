6293931ee9a6dcae358b8e25a5aed6982bd3e852
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-05 23:24:31 +0200

    added hilt to main activity

--- app/app/src/main/java/com/github/livingwithhippos/unchained/MainActivity.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/MainActivity.kt
@@ -2,7 +2,9 @@ package com.github.livingwithhippos.unchained
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
