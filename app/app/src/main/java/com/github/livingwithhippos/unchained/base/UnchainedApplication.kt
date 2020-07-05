a16bff1558074cd2e6d2cf26ca4940525165f34e
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-05 21:45:57 +0200

    added Hilt Application to manifest

---
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/base/UnchainedApplication.kt
@@ -0,0 +1,10 @@
+package com.github.livingwithhippos.unchained.base
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+//todo: move under right package
+
+@HiltAndroidApp
+class UnchainedApplication : Application() {
+}
