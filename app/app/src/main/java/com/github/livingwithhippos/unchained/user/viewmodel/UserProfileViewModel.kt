aaea38180d98541304c25eb4bcf4bde33a4a2c39
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-06 16:17:25 +0200

    added dependency injection to UserProfileViewModel

--- app/app/src/main/java/com/github/livingwithhippos/unchained/user/viewmodel/UserProfileViewModel.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/user/viewmodel/UserProfileViewModel.kt
@@ -1,9 +1,8 @@
 package com.github.livingwithhippos.unchained.user.viewmodel
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.MutableLiveData
-import com.github.livingwithhippos.unchained.base.network.ApiFactory
+import androidx.lifecycle.ViewModel
 import com.github.livingwithhippos.unchained.user.model.User
 import com.github.livingwithhippos.unchained.user.model.UserRepository
 import kotlinx.coroutines.CoroutineScope
@@ -12,26 +11,20 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 //todo: add loading of saved state
-class UserProfileViewModel(application: Application) : AndroidViewModel(application) {
+class UserProfileViewModel @ViewModelInject constructor(
+    private val userRepository: UserRepository
+) : ViewModel() {
 
 
     private val job = Job()
     val scope = CoroutineScope(Dispatchers.Default + job)
 
-    private lateinit var repository: UserRepository
-
-    fun initRepository(token: String) {
-        repository =
-            UserRepository(
-                ApiFactory(token).userApi
-            )
-    }
 
     val userLiveData = MutableLiveData<User?>()
 
     fun fetchUserInfo() {
         scope.launch {
-            val user = repository.getUserInfo()
+            val user = userRepository.getUserInfo()
             userLiveData.postValue(user)
         }
     }
