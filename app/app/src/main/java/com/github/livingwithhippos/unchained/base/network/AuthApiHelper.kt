6d2721f4977ba12d0a4828e247e7aec20d7b1a44
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-06 11:40:56 +0200

    Adde ApiHelper for D.I.

---
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/base/network/AuthApiHelper.kt
@@ -0,0 +1,21 @@
+package com.github.livingwithhippos.unchained.base.network
+
+import com.github.livingwithhippos.unchained.authentication.model.Authentication
+import com.github.livingwithhippos.unchained.authentication.model.Secrets
+import com.github.livingwithhippos.unchained.authentication.model.Token
+import retrofit2.Response
+
+interface AuthApiHelper {
+
+    suspend fun getAuthentication(): Response<Authentication>
+
+    suspend fun getSecrets(
+        deviceCode: String
+    ): Response<Secrets>
+
+    suspend fun getToken(
+        clientId: String,
+        clientSecret: String,
+        deviceCode: String
+    ): Response<Token>
+}
---
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/base/network/AuthApiHelperImpl.kt
@@ -0,0 +1,21 @@
+package com.github.livingwithhippos.unchained.base.network
+
+import com.github.livingwithhippos.unchained.authentication.model.Authentication
+import com.github.livingwithhippos.unchained.authentication.model.AuthenticationApi
+import com.github.livingwithhippos.unchained.authentication.model.Secrets
+import com.github.livingwithhippos.unchained.authentication.model.Token
+import retrofit2.Response
+import javax.inject.Inject
+
+class AuthApiHelperImpl @Inject constructor(private val authenticationApi: AuthenticationApi) : AuthApiHelper {
+
+    override suspend fun getAuthentication(): Response<Authentication> = authenticationApi.getAuthentication()
+
+    override suspend fun getSecrets(deviceCode: String): Response<Secrets> = authenticationApi.getSecrets(deviceCode = deviceCode)
+
+    override suspend fun getToken(
+        clientId: String,
+        clientSecret: String,
+        deviceCode: String
+    ): Response<Token> = authenticationApi.getToken(clientId, clientSecret, deviceCode)
+}
