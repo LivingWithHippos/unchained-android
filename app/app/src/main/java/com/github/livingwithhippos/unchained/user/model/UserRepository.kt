bb0055ff1eb517fb8ee653706b41422bbc37e281
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-06 16:16:59 +0200

    added dependency injection to UserRepository

--- app/app/src/main/java/com/github/livingwithhippos/unchained/user/model/UserRepository.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/user/model/UserRepository.kt
@@ -1,14 +1,15 @@
 package com.github.livingwithhippos.unchained.user.model
 
 import com.github.livingwithhippos.unchained.base.model.repositories.BaseRepository
+import javax.inject.Inject
 
 
-class UserRepository(private val api: UserApi) : BaseRepository() {
+class UserRepository @Inject constructor(private val userApiHelper: UserApiHelper) : BaseRepository() {
 
     suspend fun getUserInfo(): User? {
 
         val userResponse = safeApiCall(
-            call = { api.getUser() },
+            call = { userApiHelper.getUser() },
             errorMessage = "Error Fetching User Info"
         )
 
