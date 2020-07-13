f39339519702bd4bb8d07e0e47f07f149cbf1da2
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-13 13:29:31 +0200

    added fragment extension toast function

--- app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/Extension.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/Extension.kt
@@ -3,7 +3,9 @@ package com.github.livingwithhippos.unchained.utilities
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.databinding.BindingAdapter
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.github.livingwithhippos.unchained.R
 
@@ -20,4 +22,8 @@ fun AutoCompleteTextView.setAdapter(contents: List<String>) {
     // a simple layout is set for the dropdown items
     val adapter = ArrayAdapter<String>(this.context, R.layout.dropdown_plain_item, contents)
     this.setAdapter(adapter)
+}
+
+fun Fragment.showToast(stringResource: Int, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(requireContext(),getString(stringResource),length).show()
 }
