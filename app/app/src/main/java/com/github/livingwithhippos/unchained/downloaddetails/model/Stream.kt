c7bef460721ebf6cce665c4121af5d93b590e821
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-13 23:01:14 +0200

    Added Stream class and retrofit api

---
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/downloaddetails/model/Stream.kt
@@ -0,0 +1,41 @@
+package com.github.livingwithhippos.unchained.downloaddetails.model
+import com.github.livingwithhippos.unchained.authentication.model.Authentication
+import com.github.livingwithhippos.unchained.utilities.OPEN_SOURCE_CLIENT_ID
+import com.squareup.moshi.JsonClass
+
+import com.squareup.moshi.Json
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+@JsonClass(generateAdapter = true)
+data class Stream(
+    @Json(name = "apple")
+    val apple: Quality,
+    @Json(name = "dash")
+    val dash: Quality,
+    @Json(name = "liveMP4")
+    val liveMP4: Quality,
+    @Json(name = "h264WebM")
+    val h264WebM: Quality
+)
+
+@JsonClass(generateAdapter = true)
+data class Quality(
+    @Json(name = "full")
+    val link: String
+)
+
+/**
+ * The APIs in this interface will not work with an open source token.
+ */
+interface StreamingApi {
+
+    @GET(" streaming/transcode/{id}")
+    suspend fun getStreams(
+        @Header("Authorization") token: String,
+        @Path("id") id: String
+    ): Response<Stream>
+}
