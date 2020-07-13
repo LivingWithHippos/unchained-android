6bce53e992ee3e810ab081656022be765035202c
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-13 13:11:49 +0200

    added button listener in NewDownloadFragment

--- app/app/src/main/java/com/github/livingwithhippos/unchained/newdownload/view/NewDownloadFragment.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/newdownload/view/NewDownloadFragment.kt
@@ -6,9 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.github.livingwithhippos.unchained.R
 import com.github.livingwithhippos.unchained.databinding.NewDownloadFragmentBinding
 import com.github.livingwithhippos.unchained.newdownload.viewmodel.NewDownloadViewModel
+import com.github.livingwithhippos.unchained.utilities.REMOTE_TRAFFIC_OFF
+import com.github.livingwithhippos.unchained.utilities.REMOTE_TRAFFIC_ON
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -23,7 +26,23 @@ class NewDownloadFragment : Fragment() {
         val downloadBinding = NewDownloadFragmentBinding.inflate(inflater, container, false)
 
         // see AutoCompleteTextView.setAdapter(contents: List<String>) in [Extension.kt]
+        // the first value is used as default, see AutoCompleteTextView's android:text="@{list[0]}" in the layout
         downloadBinding.list = listOf(getString(R.string.no), getString(R.string.yes))
+        // add the unrestrict button listener
+        downloadBinding.bUnrestrict.setOnClickListener {
+            //todo: check acceptable values and convert them (check blank and not url etc.)
+            val link: String = downloadBinding.etLink.text.toString()
+            val password: String? = downloadBinding.etPassword.text.toString()
+            val remote: Int = if (downloadBinding.dropdownItems.text.toString()
+                    .equals(getString(R.string.yes), ignoreCase = true)
+            ) REMOTE_TRAFFIC_ON else REMOTE_TRAFFIC_OFF
+
+            viewModel.fetchUnrestrictedLink(
+                link,
+                password,
+                remote
+            )
+        }
         return downloadBinding.root
     }
 
