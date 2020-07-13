a6c4f52562f7093d5626d48bc0ccedc39eb24b8c
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-13 21:45:04 +0200

    added Fragment.copyToClipboard extension function

--- app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/Extension.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/Extension.kt
@@ -1,5 +1,8 @@
 package com.github.livingwithhippos.unchained.utilities
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.ImageView
@@ -26,4 +29,12 @@ fun AutoCompleteTextView.setAdapter(contents: List<String>) {
 
 fun Fragment.showToast(stringResource: Int, length: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(requireContext(),getString(stringResource),length).show()
+}
+
+// note: should this be added to Context instead of Fragment?
+fun Fragment.copyToClipboard(label: String, text: String) {
+    val clipboard = requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+    val clip: ClipData = ClipData.newPlainText(label, text)
+    // Set the clipboard's primary clip.
+    clipboard.setPrimaryClip(clip)
 }
