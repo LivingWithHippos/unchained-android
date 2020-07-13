372d382c2f3c38dbc6d7c7b9759cffa856eab43e
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-13 18:42:43 +0200

    load token from db if not in SavedStateHandle

--- app/app/src/main/java/com/github/livingwithhippos/unchained/newdownload/viewmodel/NewDownloadViewModel.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/newdownload/viewmodel/NewDownloadViewModel.kt
@@ -5,6 +5,7 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.github.livingwithhippos.unchained.base.model.repositories.CredentialsRepository
 import com.github.livingwithhippos.unchained.newdownload.model.UnrestrictRepository
 import com.github.livingwithhippos.unchained.newdownload.model.UnrestrictedLink
 import com.github.livingwithhippos.unchained.utilities.KEY_TOKEN
@@ -15,6 +16,7 @@ import kotlinx.coroutines.launch
 
 class NewDownloadViewModel @ViewModelInject constructor(
     @Assisted private val savedStateHandle: SavedStateHandle,
+    private val credentialsRepository: CredentialsRepository,
     private val unrestrictRepository: UnrestrictRepository
 ) : ViewModel() {
 
@@ -24,11 +26,15 @@ class NewDownloadViewModel @ViewModelInject constructor(
     val linkLiveData = MutableLiveData<UnrestrictedLink?>()
 
     fun fetchUnrestrictedLink(link: String, password: String?, remote: Int? = null) {
-        //todo: add this to fragment's argument
-        val token = savedStateHandle.get<String>(KEY_TOKEN)
-        if (token.isNullOrEmpty())
-            throw IllegalArgumentException("Loaded token was null or empty: $token")
         scope.launch {
+            //todo: add this to fragment's argument if possible
+            var token = savedStateHandle.get<String>(KEY_TOKEN)
+            if (token.isNullOrEmpty())
+                token = credentialsRepository.getCompleteCredentials().first().accessToken
+            if (token.isNullOrEmpty())
+                throw IllegalArgumentException("Loaded token was null or empty: $token")
+
+            savedStateHandle.set(KEY_TOKEN,token)
             val unrestrictedData = unrestrictRepository.getUnrestrictedLink(token, link, password, remote)
             linkLiveData.postValue(unrestrictedData)
         }
