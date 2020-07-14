6f21c1680a1b61e42a45ec0f23aa8ed0d5ff8d90
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-14 10:38:40 +0200

    committed new field by mistake

--- app/app/src/main/java/com/github/livingwithhippos/unchained/base/model/entities/Credentials.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/base/model/entities/Credentials.kt
@@ -16,7 +16,5 @@ class Credentials(
     @ColumnInfo(name = "access_token")
     val accessToken: String?,
     @ColumnInfo(name = "refresh_token")
-    val refreshToken: String?,
-    @ColumnInfo(name = "type")
-    val type: String?
+    val refreshToken: String?
 )
