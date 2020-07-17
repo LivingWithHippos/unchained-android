0e2f278fda528d75af00d9e735a06ad6666a5d53
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-17 18:46:02 +0200

    added databinding to main activity

--- app/app/src/main/java/com/github/livingwithhippos/unchained/start/view/MainActivity.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/start/view/MainActivity.kt
@@ -14,9 +14,13 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val viewModel: MainActivityViewModel by viewModels()
     }
