f054f62c5315159bafb82ad4f671a3f59bac7f27
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-28 12:19:03 +0200

    fetch torrent details and pass it to the viewmodel

--- app/app/src/main/java/com/github/livingwithhippos/unchained/torrentdetails/view/TorrentDetailsFragment.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/torrentdetails/view/TorrentDetailsFragment.kt
@@ -1,25 +1,34 @@
 package com.github.livingwithhippos.unchained.torrentdetails.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.github.livingwithhippos.unchained.R
-import com.github.livingwithhippos.unchained.databinding.FragmentDownloadDetailsBinding
-import com.github.livingwithhippos.unchained.newdownload.viewmodel.NewDownloadViewModel
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.navArgs
+import com.github.livingwithhippos.unchained.databinding.FragmentTorrentDetailsBinding
 import com.github.livingwithhippos.unchained.torrentdetails.viewmodel.TorrentDetailsViewmodel
 
 class TorrentDetailsFragment : Fragment() {
 
     private val viewModel: TorrentDetailsViewmodel by viewModels()
 
+    val args: TorrentDetailsFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val torrentBinding = FragmentDownloadDetailsBinding.inflate(inflater, container, false)
+        val torrentBinding = FragmentTorrentDetailsBinding.inflate(inflater, container, false)
+
+        viewModel.torrentLiveData.observe(viewLifecycleOwner, Observer {
+            if (!it.isNullOrEmpty())
+                torrentBinding.torrent = it.first()
+        })
+
+        viewModel.fetchTorrentDetails(args.torrentID)
         return torrentBinding.root
     }
 }
--- app/app/src/main/java/com/github/livingwithhippos/unchained/torrentdetails/viewmodel/TorrentDetailsViewmodel.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/torrentdetails/viewmodel/TorrentDetailsViewmodel.kt
@@ -2,14 +2,19 @@ package com.github.livingwithhippos.unchained.torrentdetails.viewmodel
 
 import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.github.livingwithhippos.unchained.base.model.repositories.CredentialsRepository
 import com.github.livingwithhippos.unchained.base.model.repositories.TorrentsRepository
+import com.github.livingwithhippos.unchained.newdownload.model.TorrentItem
+import com.github.livingwithhippos.unchained.newdownload.model.UnrestrictedLink
+import com.github.livingwithhippos.unchained.utilities.Event
 import com.github.livingwithhippos.unchained.utilities.KEY_TOKEN
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class TorrentDetailsViewmodel @ViewModelInject constructor(
     @Assisted private val savedStateHandle: SavedStateHandle,
@@ -20,6 +25,19 @@ class TorrentDetailsViewmodel @ViewModelInject constructor(
     private val job = Job()
     val scope = CoroutineScope(Dispatchers.Default + job)
 
+
+    val torrentLiveData = MutableLiveData<List<TorrentItem>?>()
+
+
+    fun fetchTorrentDetails(torrentID: String) {
+        scope.launch {
+            val token = getToken()
+            val torrentData =
+                torrentsRepository.getTorrentInfo(token, torrentID)
+            torrentLiveData.postValue(torrentData)
+        }
+
+    }
 
     //todo: move this to an extension, maybe passing CredentialsRepository and SavedStateHandle
     private suspend fun getToken(): String {
