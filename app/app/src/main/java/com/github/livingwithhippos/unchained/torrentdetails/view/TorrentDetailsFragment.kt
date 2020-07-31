6a229ad444523af9d00f409d3e3b240290368901
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-07-31 09:58:57 +0200

    Added more details to torrent details ui

--- app/app/src/main/java/com/github/livingwithhippos/unchained/torrentdetails/view/TorrentDetailsFragment.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/torrentdetails/view/TorrentDetailsFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
+import com.github.livingwithhippos.unchained.R
 import com.github.livingwithhippos.unchained.databinding.FragmentTorrentDetailsBinding
 import com.github.livingwithhippos.unchained.torrentdetails.viewmodel.TorrentDetailsViewmodel
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,17 +28,35 @@ class TorrentDetailsFragment : Fragment() {
     private val job = Job()
     val scope = CoroutineScope(Dispatchers.Main + job)
 
+    // possible status are magnet_error, magnet_conversion, waiting_files_selection,
+    // queued, downloading, downloaded, error, virus, compressing, uploading, dead
+    val loadingStatusList = listOf<String>("magnet_conversion","waiting_files_selection","queued", "compressing", "uploading")
+    val endedStatusList = listOf<String>("magnet_error","downloaded", "error", "virus", "dead")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val torrentBinding = FragmentTorrentDetailsBinding.inflate(inflater, container, false)
 
-        // possible status are magnet_error, magnet_conversion, waiting_files_selection,
-        // queued, downloading, downloaded, error, virus, compressing, uploading, dead
-        val loadingStatusList = listOf<String>("magnet_conversion","waiting_files_selection","queued", "compressing", "uploading")
-        val endedStatusList = listOf<String>("magnet_error","downloaded", "error", "virus", "dead")
+
+
+        val statusTranslation = mapOf<String,String>(
+            "magnet_error" to resources.getString(R.string.magnet_error),
+            "magnet_conversion" to resources.getString(R.string.magnet_conversion),
+            "waiting_files_selection" to resources.getString(R.string.waiting_files_selection),
+            "queued" to resources.getString(R.string.queued),
+            "downloading" to resources.getString(R.string.downloading),
+            "downloaded" to resources.getString(R.string.downloaded),
+            "error" to resources.getString(R.string.error),
+            "virus" to resources.getString(R.string.virus),
+            "compressing" to resources.getString(R.string.compressing),
+            "uploading" to resources.getString(R.string.uploading),
+            "dead" to resources.getString(R.string.dead)
+        )
+
         torrentBinding.loadingStatusList = loadingStatusList
+        torrentBinding.statusTranslation = statusTranslation
 
         viewModel.torrentLiveData.observe(viewLifecycleOwner, Observer {
             if (it != null) {
