eb07e9071f4b1dec806d4178613d476a85bb6d38
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-08-01 12:35:40 +0200

    Added download list fragment skeleton

---
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/downloaddetails/view/DownloadListFragment.kt
@@ -0,0 +1,21 @@
+package com.github.livingwithhippos.unchained.downloaddetails.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.livingwithhippos.unchained.R
+
+
+class DownloadListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_download_list, container, false)
+    }
+
+}
