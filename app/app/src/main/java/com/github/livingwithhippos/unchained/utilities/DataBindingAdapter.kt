28ccc820ff66e91059e3db9c00d4e5d2b23e24cc
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-08-22 23:13:10 +0200

    added basic adapter for lists

---
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/DataBindingAdapter.kt
@@ -0,0 +1,32 @@
+package com.github.livingwithhippos.unchained.utilities
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.github.livingwithhippos.unchained.BR
+
+abstract  class DataBindingAdapter<T>(diffCallback: DiffUtil.ItemCallback<T>) :
+    ListAdapter<T, DataBindingViewHolder<T>>(diffCallback){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBindingViewHolder<T> {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = DataBindingUtil.inflate<ViewDataBinding>(layoutInflater, viewType, parent, false)
+        return DataBindingViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: DataBindingViewHolder<T>, position: Int) = holder.bind(getItem(position))
+}
+
+
+class DataBindingViewHolder<T>(private val binding: ViewDataBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: T) {
+        binding.setVariable(BR.item, item)
+        binding.executePendingBindings()
+    }
+}
