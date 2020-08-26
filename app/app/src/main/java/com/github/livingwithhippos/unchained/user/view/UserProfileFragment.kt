4e376508f8f010af956e6bced56fad84e0e5f8da
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-08-26 17:50:35 +0200

    added buy premium button

--- app/app/src/main/java/com/github/livingwithhippos/unchained/user/view/UserProfileFragment.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/user/view/UserProfileFragment.kt
@@ -10,7 +10,12 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import com.github.livingwithhippos.unchained.databinding.FragmentUserProfileBinding
 import com.github.livingwithhippos.unchained.user.viewmodel.UserProfileViewModel
+import com.github.livingwithhippos.unchained.utilities.openExternalWebPage
 import dagger.hilt.android.AndroidEntryPoint
+
+
+const val REFERRAL_LINK = "http://real-debrid.com/?id=78841"
+const val PREMIUM_LINK = "https://real-debrid.com/premium"
 
 /**
  * A simple [Fragment] subclass.
@@ -46,6 +51,12 @@ class UserProfileFragment : Fragment() {
 
             //todo: manage null
         })
+
+        userBinding.bPremium.setOnClickListener {
+            //todo: ask user and either load the referral link
+            // or the premium page, add to settings fragment
+            openExternalWebPage(REFERRAL_LINK)
+        }
 
         return userBinding.root
     }
