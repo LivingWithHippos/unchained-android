5ccc5a0a86aecc9a97835feb8545ac69c8809ebf
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-08-31 11:07:39 +0200

    added new methods to return DownloadList to api helpers

--- app/app/src/main/java/com/github/livingwithhippos/unchained/downloadlists/model/DownloadApiHelper.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/downloadlists/model/DownloadApiHelper.kt
@@ -9,4 +9,18 @@ interface DownloadApiHelper {
         page: Int,
         limit: Int
     ): Response<List<DownloadItem>>
+
+    suspend fun getDownloadsList(
+        token: String,
+        offset: Int,
+        page: Int,
+        limit: Int
+    ): Response<DownloadList>
+
+    suspend fun getDownloadsDirectly(
+        token: String,
+        offset: Int,
+        page: Int,
+        limit: Int
+    ): DownloadList
 }
--- app/app/src/main/java/com/github/livingwithhippos/unchained/downloadlists/model/DownloadApiHelperImpl.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/downloadlists/model/DownloadApiHelperImpl.kt
@@ -1,10 +1,12 @@
 package com.github.livingwithhippos.unchained.downloadlists.model
 
+import com.github.livingwithhippos.unchained.base.model.repositories.BaseRepository
 import retrofit2.Response
 import javax.inject.Inject
 
-class DownloadApiHelperImpl @Inject constructor(private val downloadsApi: DownloadsApi) :
+class DownloadApiHelperImpl @Inject constructor(private val downloadsApi: DownloadsApi) : BaseRepository(),
     DownloadApiHelper {
+
     override suspend fun getDownloads(
         token: String,
         offset: Int,
@@ -12,4 +14,27 @@ class DownloadApiHelperImpl @Inject constructor(private val downloadsApi: Downlo
         limit: Int
     ): Response<List<DownloadItem>> =
         downloadsApi.getDownloads(token, offset, page, limit)
+
+    override suspend fun getDownloadsList(
+        token: String,
+        offset: Int,
+        page: Int,
+        limit: Int
+    ): Response<DownloadList> =
+        downloadsApi.getDownloadsList(token, offset, page, limit)
+
+    override suspend fun getDownloadsDirectly(
+        token: String,
+        offset: Int,
+        page: Int,
+        limit: Int
+    ): DownloadList {
+
+        val downloadResponse = safeApiCall(
+            call = { getDownloadsList("Bearer $token", offset, page, limit) },
+            errorMessage = "Error Fetching User Info"
+        )
+
+        return downloadResponse ?: DownloadList(emptyList())
+    }
 }
