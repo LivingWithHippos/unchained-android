5f3fa8d485fcd603769b466feaa6aaa749173e81
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-08-31 12:48:10 +0200

    added DataBindingPagingAdapter class
    paging alternative to the base databinding list adapter

--- app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/DataBindingAdapter.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/DataBindingAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -22,6 +23,23 @@ abstract  class DataBindingAdapter<T, U>(diffCallback: DiffUtil.ItemCallback<T>,
     override fun onBindViewHolder(holder: DataBindingViewHolder<T,U>, position: Int) = holder.bind(getItem(position), listener)
 }
 
+abstract  class DataBindingPagingAdapter<T: Any, U>(diffCallback: DiffUtil.ItemCallback<T>, val listener: U? = null) :
+    PagingDataAdapter<T, DataBindingViewHolder<T, U>>(diffCallback){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBindingViewHolder<T, U> {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = DataBindingUtil.inflate<ViewDataBinding>(layoutInflater, viewType, parent, false)
+        return DataBindingViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: DataBindingViewHolder<T,U>, position: Int) {
+        val repoItem = getItem(position)
+        if (repoItem != null) {
+            holder.bind(repoItem, listener)
+        }
+
+    }
+}
 
 class DataBindingViewHolder<T,U>(private val binding: ViewDataBinding) :
     RecyclerView.ViewHolder(binding.root) {
