6e0c4e98cdeef0ffca2a79bb157947584c10c8a9
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-09-11 12:32:39 +0200

    removed unused paging method

--- app/app/src/main/java/com/github/livingwithhippos/unchained/base/model/repositories/DownloadRepository.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/base/model/repositories/DownloadRepository.kt
@@ -1,15 +1,7 @@
 package com.github.livingwithhippos.unchained.base.model.repositories
 
-import androidx.lifecycle.LiveData
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import androidx.paging.liveData
 import com.github.livingwithhippos.unchained.downloadlists.model.DownloadApiHelper
 import com.github.livingwithhippos.unchained.downloadlists.model.DownloadItem
-import com.github.livingwithhippos.unchained.downloadlists.model.DownloadList
-import com.github.livingwithhippos.unchained.downloadlists.model.DownloadPagingSource
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class DownloadRepository @Inject constructor(private val downloadApiHelper: DownloadApiHelper) :
@@ -28,20 +20,6 @@ class DownloadRepository @Inject constructor(private val downloadApiHelper: Down
 
         return downloadResponse ?: emptyList<DownloadItem>()
 
-    }
-
-    fun getDownloadListLiveData(query: String): LiveData<PagingData<DownloadList>> {
-        return Pager(
-            config = PagingConfig(
-                pageSize = NETWORK_PAGE_SIZE,
-                enablePlaceholders = false
-            ),
-            pagingSourceFactory = { DownloadPagingSource(downloadApiHelper, query) }
-        ).liveData
-    }
-
-    companion object {
-        private const val NETWORK_PAGE_SIZE = 50
     }
 
 }
