5d5fcd95e87f817ad8fd2350087a230deac38ff6
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-09-11 15:56:01 +0200

    Create DownloadListPagingAdapter.kt
    same as the old one but extends DataBindingPagingAdapter

---
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/downloadlists/view/DownloadListPagingAdapter.kt
@@ -0,0 +1,20 @@
+package com.github.livingwithhippos.unchained.downloadlists.view
+import androidx.recyclerview.widget.DiffUtil
+import com.github.livingwithhippos.unchained.R
+import com.github.livingwithhippos.unchained.downloadlists.model.DownloadItem
+import com.github.livingwithhippos.unchained.utilities.DataBindingPagingAdapter
+
+class DownloadListPagingAdapter(listener: DownloadListListener) : DataBindingPagingAdapter<DownloadItem, DownloadListListener>(DiffCallback(), listener) {
+
+    class DiffCallback : DiffUtil.ItemCallback<DownloadItem>() {
+        override fun areItemsTheSame(oldItem: DownloadItem, newItem: DownloadItem): Boolean =
+            oldItem.id==newItem.id
+
+        override fun areContentsTheSame(oldItem: DownloadItem, newItem: DownloadItem): Boolean {
+            //todo: add progress if present
+            return oldItem.link == newItem.link
+        }
+    }
+
+    override fun getItemViewType(position: Int) = R.layout.item_list_download
+}
