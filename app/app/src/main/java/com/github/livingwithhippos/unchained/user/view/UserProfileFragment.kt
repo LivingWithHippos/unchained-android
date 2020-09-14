890304c419f98bd6c78bebc1ac54845de6a42c10
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-09-14 11:11:56 +0200

    remove unused factory method

--- app/app/src/main/java/com/github/livingwithhippos/unchained/user/view/UserProfileFragment.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/user/view/UserProfileFragment.kt
@@ -19,8 +19,6 @@ const val PREMIUM_LINK = "https://real-debrid.com/premium"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [UserProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
  */
 @AndroidEntryPoint
 class UserProfileFragment : Fragment() {
@@ -58,19 +56,5 @@ class UserProfileFragment : Fragment() {
             openExternalWebPage(REFERRAL_LINK)
         }
         return userBinding.root
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment.
-         *
-         * @return A new instance of fragment UserProfile.
-         */
-        @JvmStatic
-        fun newInstance() =
-            UserProfileFragment()
-                .apply {
-                }
     }
 }
