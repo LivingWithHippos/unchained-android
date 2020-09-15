cbcf0b309ddd5538d8e26f1a69bc5ead21add91c
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-09-15 12:01:13 +0200

    testing exceptions for unrestrict api

--- app/app/src/main/java/com/github/livingwithhippos/unchained/newdownload/view/NewDownloadFragment.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/newdownload/view/NewDownloadFragment.kt
@@ -18,6 +18,7 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.github.livingwithhippos.unchained.R
+import com.github.livingwithhippos.unchained.base.UnchainedFragment
 import com.github.livingwithhippos.unchained.databinding.NewDownloadFragmentBinding
 import com.github.livingwithhippos.unchained.newdownload.viewmodel.NewDownloadViewModel
 import com.github.livingwithhippos.unchained.torrentdetails.view.TorrentDetailsFragmentArgs
@@ -36,7 +37,7 @@ import java.util.regex.Pattern
 
 
 @AndroidEntryPoint
-class NewDownloadFragment : Fragment(), NewDownloadListener {
+class NewDownloadFragment : UnchainedFragment(), NewDownloadListener {
 
     private val viewModel: NewDownloadViewModel by viewModels()
 
@@ -69,6 +70,64 @@ class NewDownloadFragment : Fragment(), NewDownloadListener {
                 findNavController().navigate(action)
             }
         })
+
+        viewModel.apiErrorLiveData.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let { error ->
+                when (error.errorCode) {
+                    -1 -> showToast(R.string.internal_error)
+                    1 -> showToast(R.string.missing_parameter)
+                    2 -> showToast(R.string.bad_parameter_value)
+                    3 -> showToast(R.string.unknown_method)
+                    4 -> showToast(R.string.method_not_allowed)
+                    // what is this error for?
+                    5 -> showToast(R.string.slow_down)
+                    6 -> showToast(R.string.resource_unreachable)
+                    7 -> showToast(R.string.resource_not_found)
+                    //todo: check these
+                    8 -> {
+                        showToast(R.string.bad_token)
+                        activityViewModel.setUnauthenticated()
+                    }
+                    9 -> showToast(R.string.permission_denied)
+                    10 -> showToast(R.string.tfa_needed)
+                    11 -> showToast(R.string.tfa_pending)
+                    12 -> {
+                        showToast(R.string.invalid_login)
+                        activityViewModel.setUnauthenticated()
+                    }
+                    13 -> showToast(R.string.invalid_password)
+                    14 -> {
+                        showToast(R.string.account_locked)
+                        activityViewModel.setUnauthenticated()
+                    }
+                    15 -> showToast(R.string.account_not_activated)
+                    16 -> showToast(R.string.unsupported_hoster)
+                    17 -> showToast(R.string.hoster_in_maintenance)
+                    18 -> showToast(R.string.hoster_limit_reached)
+                    19 -> showToast(R.string.hoster_temporarily_unavailable)
+                    20 -> showToast(R.string.hoster_not_available_for_free_users)
+                    21 -> showToast(R.string.too_many_active_downloads)
+                    22 -> showToast(R.string.ip_Address_not_allowed)
+                    23 -> showToast(R.string.traffic_exhausted)
+                    24 -> showToast(R.string.file_unavailable)
+                    25 -> showToast(R.string.service_unavailable)
+                    26 -> showToast(R.string.upload_too_big)
+                    27 -> showToast(R.string.upload_error)
+                    28 -> showToast(R.string.file_not_allowed)
+                    29 -> showToast(R.string.torrent_too_big)
+                    30 -> showToast(R.string.torrent_file_invalid)
+                    31 -> showToast(R.string.action_already_done)
+                    32 -> showToast(R.string.image_resolution_error)
+                    33 -> showToast(R.string.torrent_already_active)
+                    else -> showToast(R.string.error_unrestricting_download)
+                }
+                // re enable buttons to let the user take other actions
+                //todo: this needs to be done also for other errors. maybe throw another error from the viewmodel
+                downloadBinding.bUnrestrict.isEnabled = true
+                downloadBinding.bLoadTorrent.isEnabled = true
+            }
+        })
+
         // add the unrestrict button listener
         downloadBinding.bUnrestrict.setOnClickListener {
             val link: String = downloadBinding.tiLink.text.toString().trim()
