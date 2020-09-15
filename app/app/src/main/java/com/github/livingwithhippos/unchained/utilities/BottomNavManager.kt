26d4219429094a7a2202543cbbe486bd1f2c1eb3
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-09-15 23:58:12 +0200

    removed unused variables
    also formatted

--- app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/BottomNavManager.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/BottomNavManager.kt
@@ -21,9 +21,9 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
  */
 
 class BottomNavManager(
-    private val fragmentManager: FragmentManager,
-    private val containerId: Int,
-    private val bottomNavigationView: BottomNavigationView
+        private val fragmentManager: FragmentManager,
+        private val containerId: Int,
+        private val bottomNavigationView: BottomNavigationView
 ) {
 
     companion object {
@@ -32,7 +32,7 @@ class BottomNavManager(
 
     // Graph Id's of the tabs
     private val navGraphIds =
-        listOf(R.navigation.home_nav_graph, R.navigation.download_nav_graph,  R.navigation.lists_nav_graph)
+            listOf(R.navigation.home_nav_graph, R.navigation.download_nav_graph, R.navigation.lists_nav_graph)
 
     // Map of tags
     private val graphIdToTagMap = SparseArray<String>()
@@ -70,14 +70,14 @@ class BottomNavManager(
 
             // Find or create the Navigation host fragment
             val navHostFragment = obtainNavHostFragment(
-                fragmentTag,
-                navGraphId
+                    fragmentTag,
+                    navGraphId
             )
 
             // Obtain its id
             val graphId = navHostFragment.navController.graph.id
             navGraphStartDestinations[graphId] =
-                navHostFragment.navController.graph.startDestination
+                    navHostFragment.navController.graph.startDestination
 
             // Save to the map
             graphIdToTagMap[graphId] = fragmentTag
@@ -93,7 +93,7 @@ class BottomNavManager(
         }
     }
 
-    fun disableMenuItems(exceptions: List<Int>= emptyList()) {
+    fun disableMenuItems(exceptions: List<Int> = emptyList()) {
         bottomNavigationView.disableItems(exceptions)
     }
 
@@ -119,20 +119,20 @@ class BottomNavManager(
                             as NavHostFragment
 
                     fragmentManager.beginTransaction()
-                        .show(selectedFragment)
-                        .setMaxLifecycle(selectedFragment, Lifecycle.State.RESUMED)
-                        .setPrimaryNavigationFragment(selectedFragment)
-                        .apply {
-                            // Detach all other Fragments
-                            graphIdToTagMap.forEach { _, fragmentTag ->
-                                if (fragmentTag != newlySelectedItemTag) {
-                                    val fragment = fragmentManager.findFragmentByTag(fragmentTag)!!
-                                    hide(fragment)
-                                    setMaxLifecycle(fragment, Lifecycle.State.STARTED)
+                            .show(selectedFragment)
+                            .setMaxLifecycle(selectedFragment, Lifecycle.State.RESUMED)
+                            .setPrimaryNavigationFragment(selectedFragment)
+                            .apply {
+                                // Detach all other Fragments
+                                graphIdToTagMap.forEach { _, fragmentTag ->
+                                    if (fragmentTag != newlySelectedItemTag) {
+                                        val fragment = fragmentManager.findFragmentByTag(fragmentTag)!!
+                                        hide(fragment)
+                                        setMaxLifecycle(fragment, Lifecycle.State.STARTED)
+                                    }
                                 }
                             }
-                        }
-                        .commit()
+                            .commit()
 
                     selectedNavController = selectedFragment.navController
                 }
@@ -159,17 +159,15 @@ class BottomNavManager(
     // select particular bottom navigation item
     fun selectItem(itemId: Int) {
         bottomNavigationView.menu.findItem(itemId)
-            ?.let {
-                bottomNavigationView.menu.performIdentifierAction(itemId, 0)
-            }
+                ?.let {
+                    bottomNavigationView.menu.performIdentifierAction(itemId, 0)
+                }
     }
 
     // controls the back press mechanism
     fun onBackPressed(): Boolean {
         return if (navHistory.isNotEmpty) {
             selectedNavController?.let {
-                val cd = it.currentDestination?.id
-                val sid = navGraphStartDestinations[bottomNavigationView.selectedItemId]
                 if (it.currentDestination == null || it.currentDestination?.id == navGraphStartDestinations[bottomNavigationView.selectedItemId]) {
                     if (isFirstTab()) return false
 
@@ -204,25 +202,25 @@ class BottomNavManager(
     }
 
     private fun showNavHostFragment(
-        navHostFragment: NavHostFragment,
-        show: Boolean
+            navHostFragment: NavHostFragment,
+            show: Boolean
     ) {
         fragmentManager.beginTransaction()
-            .apply {
-                if (show) {
-                    show(navHostFragment)
-                    setMaxLifecycle(navHostFragment, Lifecycle.State.RESUMED)
-                } else {
-                    hide(navHostFragment)
-                    setMaxLifecycle(navHostFragment, Lifecycle.State.STARTED)
+                .apply {
+                    if (show) {
+                        show(navHostFragment)
+                        setMaxLifecycle(navHostFragment, Lifecycle.State.RESUMED)
+                    } else {
+                        hide(navHostFragment)
+                        setMaxLifecycle(navHostFragment, Lifecycle.State.STARTED)
+                    }
                 }
-            }
-            .commitNow()
+                .commitNow()
     }
 
     private fun obtainNavHostFragment(
-        fragmentTag: String,
-        navGraphId: Int
+            fragmentTag: String,
+            navGraphId: Int
     ): NavHostFragment {
         // If the Nav Host fragment exists, return it
         val existingFragment = fragmentManager.findFragmentByTag(fragmentTag) as NavHostFragment?
@@ -231,8 +229,8 @@ class BottomNavManager(
         // Otherwise, create it and return it.
         val navHostFragment = NavHostFragment.create(navGraphId)
         fragmentManager.beginTransaction()
-            .add(containerId, navHostFragment, fragmentTag)
-            .commitNow()
+                .add(containerId, navHostFragment, fragmentTag)
+                .commitNow()
         return navHostFragment
     }
 
