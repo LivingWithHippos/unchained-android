89fd527f9c06311d737536c1e53f72f03b0f4c9a
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-09-21 22:42:23 +0200

    moved download list to tab layout fragment

--- app/app/src/main/java/com/github/livingwithhippos/unchained/lists/view/ListsTabFragment.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/lists/view/ListsTabFragment.kt
@@ -4,12 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.paging.PagingData
 import com.github.livingwithhippos.unchained.base.UnchainedFragment
 import com.github.livingwithhippos.unchained.databinding.TablayoutListBinding
+import com.github.livingwithhippos.unchained.downloadlists.model.DownloadItem
+import com.github.livingwithhippos.unchained.downloadlists.view.DownloadListFragmentDirections
+import com.github.livingwithhippos.unchained.downloadlists.view.DownloadListListener
+import com.github.livingwithhippos.unchained.downloadlists.view.DownloadListPagingAdapter
+import com.github.livingwithhippos.unchained.downloadlists.viewmodel.DownloadListViewModel
+import com.github.livingwithhippos.unchained.start.viewmodel.MainActivityViewModel
 import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-class ListsTabFragment: UnchainedFragment() {
+@AndroidEntryPoint
+class ListsTabFragment: UnchainedFragment(), DownloadListListener {
+
+    private val viewModel: DownloadListViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -38,6 +53,38 @@ class ListsTabFragment: UnchainedFragment() {
             }
         })
 
+        val adapter = DownloadListPagingAdapter(this)
+        listBinding.rvDownloadList.adapter = adapter
+
+        //todo: add scroll to top when a new item is added
+        listBinding.srLayout.setOnRefreshListener {
+            adapter.refresh()
+        }
+
+        // observer created to easily add and remove it. Pass the retrieved download list to the adapter and removes the loading icon from the swipe layout
+        val downloadObserver = Observer<PagingData<DownloadItem>> {
+            lifecycleScope.launch {
+                adapter.submitData(it)
+                listBinding.srLayout.isRefreshing = false
+            }
+        }
+
+        // checks the authentication state. Needed to avoid automatic API calls before the authentication process is finished
+        activityViewModel.authenticationState.observe(viewLifecycleOwner, Observer {
+            if (it.peekContent() == MainActivityViewModel.AuthenticationState.AUTHENTICATED) {
+                // register observer if not already registered
+                if (!viewModel.listData.hasActiveObservers())
+                    viewModel.listData.observe(viewLifecycleOwner, downloadObserver)
+            } else {
+                // remove observer if present
+                viewModel.listData.removeObserver(downloadObserver)
+            }
+        })
+
         return listBinding.root
     }
+
+    override fun onClick(item: DownloadItem) {
+    }
+
 }
