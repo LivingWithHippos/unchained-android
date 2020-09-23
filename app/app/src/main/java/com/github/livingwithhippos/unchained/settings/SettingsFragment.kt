04ac447404cd248ffbe896e4fc19b5ebaef59d1d
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2020-09-23 13:26:03 +0200

    added credits and terms dialog

--- app/app/src/main/java/com/github/livingwithhippos/unchained/settings/SettingsFragment.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/settings/SettingsFragment.kt
@@ -7,6 +7,7 @@ import com.github.livingwithhippos.unchained.R
 import com.github.livingwithhippos.unchained.utilities.FEEDBACK_URL
 import com.github.livingwithhippos.unchained.utilities.GPLV3_URL
 import com.github.livingwithhippos.unchained.utilities.openExternalWebPage
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -19,9 +20,35 @@ class SettingsFragment : PreferenceFragmentCompat() {
         when (preference?.key) {
             "feedback" -> openExternalWebPage(FEEDBACK_URL)
             "license" -> openExternalWebPage(GPLV3_URL)
+            "credits" -> {
+                openCreditsDialog()
+            }
+            "terms" -> {
+                openTermsDialog()
+            }
             else -> return super.onPreferenceTreeClick(preference)
         }
 
         return true
+    }
+
+    private fun openCreditsDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(resources.getString(R.string.credits_title))
+            .setMessage(resources.getString(R.string.credits_text))
+            .setNeutralButton(resources.getString(R.string.close)) { dialog, _ ->
+                dialog.cancel()
+            }
+            .show()
+    }
+
+    private fun openTermsDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(resources.getString(R.string.terms_title))
+            .setMessage(resources.getString(R.string.terms_text))
+            .setNeutralButton(resources.getString(R.string.close)) { dialog, _ ->
+                dialog.cancel()
+            }
+            .show()
     }
 }
