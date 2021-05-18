5da9feb752e6e17e2d71a5a1d41be060a00b9d46
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2021-05-18 14:09:26 +0200

    new repository functions for regex type

--- app/app/src/main/java/com/github/livingwithhippos/unchained/data/repositoy/HostsRepository.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/data/repositoy/HostsRepository.kt
@@ -3,6 +3,8 @@ package com.github.livingwithhippos.unchained.data.repositoy
 import com.github.livingwithhippos.unchained.data.local.HostRegexDao
 import com.github.livingwithhippos.unchained.data.model.Host
 import com.github.livingwithhippos.unchained.data.model.HostRegex
+import com.github.livingwithhippos.unchained.data.model.REGEX_TYPE_FOLDER
+import com.github.livingwithhippos.unchained.data.model.REGEX_TYPE_HOST
 import com.github.livingwithhippos.unchained.data.remote.HostsApiHelper
 import java.util.regex.Pattern
 import java.util.regex.PatternSyntaxException
@@ -40,8 +42,28 @@ class HostsRepository @Inject constructor(
         // add the regexps from the network
         hostResponse?.forEach {
             val regex = convertRegex(it)
-            if (!regex.isBlank())
-                list.add(HostRegex(regex))
+            if (regex.isNotBlank())
+                list.add(HostRegex(regex, type = REGEX_TYPE_HOST))
+        }
+        return list
+    }
+
+    /**
+     * Gets the regexps to filter supported hosts folders from the network.
+     * @return the list of HostRegex from the network
+     */
+    private suspend fun getFoldersRegexFromNetwork(): List<HostRegex> {
+
+        val hostResponse = safeApiCall(
+            call = { hostsApiHelper.getHostsFoldersRegex() },
+            errorMessage = "Error Fetching Hosts Folders Regex"
+        )
+        val list = mutableListOf<HostRegex>()
+        // add the regexps from the network
+        hostResponse?.forEach {
+            val regex = convertRegex(it)
+            if (regex.isNotBlank())
+                list.add(HostRegex(regex, type = REGEX_TYPE_FOLDER))
         }
         return list
     }
@@ -53,7 +75,7 @@ class HostsRepository @Inject constructor(
     suspend fun getHostsRegex(addCustomRegexps: Boolean = true): List<HostRegex> {
 
         val regexps = mutableListOf<HostRegex>()
-        regexps.addAll(hostRegexDao.getAllRegexps())
+        regexps.addAll(hostRegexDao.getHostRegexps())
         if (regexps.size < 10) {
             regexps.clear()
             regexps.addAll(updateHostsRegex())
@@ -74,7 +96,43 @@ class HostsRepository @Inject constructor(
     suspend fun updateHostsRegex(): List<HostRegex> {
         val newRegexps = getHostsRegexFromNetwork()
         if (newRegexps.size > 10) {
-            hostRegexDao.deleteAll()
+            hostRegexDao.deleteAllHosts()
+            hostRegexDao.insertAll(newRegexps)
+            return newRegexps
+        } else
+            return emptyList()
+    }
+
+
+    /**
+     * Gets the regexps to filter supported hosts from the db if any, otherwise tries to update them from the network
+     * @return the list of HostRegex from the db or network, or an empty list
+     */
+    suspend fun getFoldersRegex(addCustomRegexps: Boolean = true): List<HostRegex> {
+
+        val regexps = mutableListOf<HostRegex>()
+        regexps.addAll(hostRegexDao.getFoldersRegexps())
+        if (regexps.size < 10) {
+            regexps.clear()
+            regexps.addAll(updateFoldersRegex())
+        }
+
+        // add the custom regexps
+        if (addCustomRegexps)
+            regexps.addAll(CUSTOM_FOLDER_REGEXPS.map { HostRegex(it) })
+
+        return regexps
+
+    }
+
+    /**
+     * Gets the regexps to filter supported hosts from the network and saves them in the local database, deleting the old ones
+     * @return the list of HostRegex saved in the database, or an empty list
+     */
+    suspend fun updateFoldersRegex(): List<HostRegex> {
+        val newRegexps = getFoldersRegexFromNetwork()
+        if (newRegexps.size > 10) {
+            hostRegexDao.deleteAllFolders()
             hostRegexDao.insertAll(newRegexps)
             return newRegexps
         } else
@@ -110,5 +168,9 @@ class HostsRepository @Inject constructor(
             "^https?:\\/\\/(www?\\d?\\.)?rapidgator\\.(net|asia)\\/file\\/[0-9a-z]{6,32}/([^(\\/| |\"|'|>|<|\\r\\n\\|\\r|\\n|:|\$)]+)\$",
             "^(https?:\\/\\/)?(www?\\d?\\.)?(m\\.)?youtu(be)?\\.(com|be)\\/([^(|\"|'|>|<|\\s|\\r\\n\\|\\r|\\n|:|\$)]+)\$"
         )
+
+        // if any of the converted folder regexps are wrong, we can add these to the db manually
+        val CUSTOM_FOLDER_REGEXPS = emptyArray<String>()
+
     }
 }
