5e4ed3da841159fd7b46e4712988a39b4d5b60e6
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2021-06-11 01:16:15 +0200

    set up dynamic url for kodi api

--- app/app/src/main/java/com/github/livingwithhippos/unchained/data/remote/KodiApiHelperImpl.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/data/remote/KodiApiHelperImpl.kt
@@ -6,7 +6,7 @@ import retrofit2.Response
 import javax.inject.Inject
 
 
-class KodiApiHelperImpl @Inject constructor(private val kodiApi: KodiApi) : KodiApiHelper {
+class KodiApiHelperImpl(private val kodiApi: KodiApi) : KodiApiHelper {
     override suspend fun openUrl(request: KodiOpenRequest): Response<KodiResponse> =
         kodiApi.openUrl(request)
 }
--- app/app/src/main/java/com/github/livingwithhippos/unchained/data/repositoy/KodiRepository.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/data/repositoy/KodiRepository.kt
@@ -3,24 +3,49 @@ package com.github.livingwithhippos.unchained.data.repositoy
 import com.github.livingwithhippos.unchained.data.model.KodiItem
 import com.github.livingwithhippos.unchained.data.model.KodiOpenRequest
 import com.github.livingwithhippos.unchained.data.model.KodiParams
+import com.github.livingwithhippos.unchained.data.remote.KodiApi
 import com.github.livingwithhippos.unchained.data.remote.KodiApiHelper
+import com.github.livingwithhippos.unchained.data.remote.KodiApiHelperImpl
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Inject
 
 class KodiRepository @Inject constructor(
-    private val kodiApiHelper: KodiApiHelper
+    private val client: OkHttpClient
 ) : BaseRepository() {
 
-    suspend fun playUrl(url: String) {
+    private fun provideRetrofit(baseUrl: String): Retrofit {
+        return Retrofit.Builder()
+            .client(client)
+            .baseUrl(baseUrl)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    private fun provideApi(baseUrl: String): KodiApi {
+        return provideRetrofit(baseUrl).create(KodiApi::class.java)
+    }
+
+    private fun provideApiHelper(baseUrl: String): KodiApiHelper =
+        KodiApiHelperImpl(provideApi(baseUrl))
+
+    suspend fun openUrl(baseUrl: String, url: String) {
+
+        val kodiApiHelper: KodiApiHelper = provideApiHelper(baseUrl)
+
         val kodiResponse = safeApiCall(
-            call = { kodiApiHelper.openUrl(
-                KodiOpenRequest(
-                    params = KodiParams(
-                        item = KodiItem(
-                            fileUrl = url
+            call = {
+                kodiApiHelper.openUrl(
+                    KodiOpenRequest(
+                        params = KodiParams(
+                            item = KodiItem(
+                                fileUrl = url
+                            )
                         )
                     )
                 )
-            ) },
+            },
             errorMessage = "Error Sending url to Kodi"
         )
 
