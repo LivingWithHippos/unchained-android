e6adb250cd59cd1fbd7080e86de909d24bed74d4
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2021-08-19 02:28:32 +0200

    Squashed commit of the following:
    commit 1d85c2eb7243e3827cb1f32421376d0e4ae23ad6
    Author: LivingWithHippos <LivingWithHippos@users.noreply.github.com>
    Date:   Thu Aug 19 02:28:16 2021 +0200
    
        format code
    
    commit 95e7051f87cd1bba9f676f776b2839688c7d8411
    Author: LivingWithHippos <LivingWithHippos@users.noreply.github.com>
    Date:   Thu Aug 19 02:24:05 2021 +0200
    
        added a link to the plugins page from the search screen

--- app/app/src/main/java/com/github/livingwithhippos/unchained/search/view/SearchFragment.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/search/view/SearchFragment.kt
@@ -3,6 +3,9 @@ package com.github.livingwithhippos.unchained.search.view
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
@@ -35,6 +38,27 @@ import timber.log.Timber
 class SearchFragment : UnchainedFragment(), SearchItemListener {
 
     private val viewModel: SearchViewModel by viewModels()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_bar, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.plugins_link -> {
+                openExternalWebPage(PLUGINS_URL)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
