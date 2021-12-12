25e470e3efcbb00b9b3157b1772a2d85660532bd
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2021-12-12 14:16:26 +0100

    updated downloader to handle streams

--- app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/Downloader.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/Downloader.kt
@@ -2,19 +2,27 @@ package com.github.livingwithhippos.unchained.utilities
 
 import com.github.livingwithhippos.unchained.di.ClassicClient
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import timber.log.Timber
-import java.io.File
+import java.io.OutputStream
 import javax.inject.Inject
 
 
 class Downloader @Inject constructor(@ClassicClient private val okHttpClient: OkHttpClient) {
 
-    suspend fun downloadFileViaOKHTTP(url: String, destinationFile: File) = withContext(Dispatchers.IO) {
+    private val _progress: MutableStateFlow<Int> = MutableStateFlow(0)
+    val progress: StateFlow<Int> get() = _progress
+
+    suspend fun downloadFileViaOKHTTP(url: String, outputStream: OutputStream) = withContext(Dispatchers.IO) {
+        _progress.value = 0
+
         val request: Request = Request.Builder().url(url).build()
 
         val response: Response = okHttpClient.newCall(request).execute()
@@ -24,15 +32,17 @@ class Downloader @Inject constructor(@ClassicClient private val okHttpClient: Ok
         val length: Double = response.header("Content-Length", "1")?.toDouble() ?: 1.toDouble()
 
         val customInputStream = FlowableInputStream(responseBody.byteStream())
-        customInputStream.counter.collect {
-            val progress = it * 100 / length
-            Timber.d("progress: $progress")
+        launch {
+            customInputStream.counter.collect {
+                _progress.value = (it * 100 / length).toInt()
+            }
         }
 
         customInputStream.use { input ->
-            destinationFile.outputStream().use { output ->
-                input.copyTo(output)
+            outputStream.use { output ->
+                input.copyTo(output, 1024)
             }
         }
+        // code written down here will wait for the download to complete
     }
 }
