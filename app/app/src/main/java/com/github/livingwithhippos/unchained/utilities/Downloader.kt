d39ec4cff1ef065cbea4b92f703e575d9bdf1306
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2021-12-21 16:17:24 +0100

    added pause and stop features

--- app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/Downloader.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/Downloader.kt
@@ -2,6 +2,7 @@ package com.github.livingwithhippos.unchained.utilities
 
 import com.github.livingwithhippos.unchained.di.ClassicClient
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
@@ -10,7 +11,6 @@ import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
-import timber.log.Timber
 import java.io.OutputStream
 import javax.inject.Inject
 
@@ -20,9 +20,14 @@ class Downloader @Inject constructor(@ClassicClient private val okHttpClient: Ok
     private val _downloadInfo: MutableStateFlow<Pair<Double,Double>> = MutableStateFlow(Pair(0.0,0.0))
     val downloadInfo: StateFlow<Pair<Double, Double>> get() = _downloadInfo
 
+    var pauseDownload = false
+    var endDownload = false
+
     suspend fun downloadFileViaOKHTTP(url: String, outputStream: OutputStream) = withContext(Dispatchers.IO) {
-        // reset the variable value
+        // reset the variables values
         _downloadInfo.value = Pair(0.0,0.0)
+        pauseDownload = false
+        endDownload = false
 
         val request: Request = Request.Builder().url(url).build()
 
@@ -39,9 +44,28 @@ class Downloader @Inject constructor(@ClassicClient private val okHttpClient: Ok
             }
         }
 
-        customInputStream.use { input ->
-            outputStream.use { output ->
-                input.copyTo(output, 1024)
+        customInputStream.use input@{ input ->
+            outputStream.use output@{ output ->
+                // taken from InputStream.copyTo
+                var bytesCopied: Long = 0
+                val buffer = ByteArray(1024)
+                var bytes = input.read(buffer)
+                while (bytes >= 0) {
+                    when {
+                        endDownload -> {
+                            return@output bytesCopied
+                        }
+                        pauseDownload -> {
+                            delay(1000)
+                        }
+                        else -> {
+                            output.write(buffer, 0, bytes)
+                            bytesCopied += bytes
+                            bytes = input.read(buffer)
+                        }
+                    }
+                }
+                return@output bytesCopied
             }
         }
         // code written below here will wait for the download to complete
