83500d6b72135510810e4bf28dac4d2108fd3f09
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2022-05-15 21:04:36 +0200

    added simple tests for login/logout

--- app/app/src/androidTest/java/com/github/livingwithhippos/unchained/base/MainActivityTest.kt
+++ app/app/src/androidTest/java/com/github/livingwithhippos/unchained/base/MainActivityTest.kt
@@ -1,19 +1,23 @@
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withParent
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
 import com.github.livingwithhippos.unchained.R
 import com.github.livingwithhippos.unchained.base.MainActivity
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.core.IsInstanceOf
+import com.github.livingwithhippos.unchained.base.waitForView
+import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -23,14 +27,40 @@ class MainActivityTest {
     @JvmField
     var rule = ActivityScenarioRule(MainActivity::class.java)
 
+    /*
+
     @Test
     fun mainActivityTest() {
-        val checkBox = onView(
-            allOf(
-                withId(R.id.cbLink),
-                isDisplayed()
-            )
-        )
-        checkBox.check(matches(isDisplayed()))
+            launchActivity<MainActivity>().use { scenario ->
+                scenario.moveToState(Lifecycle.State.CREATED)
+            }
+        }
+
+     */
+
+    @Test
+    fun logoutTest() {
+        // if the app is already logged in this is going to fail
+        onView(isRoot()).perform(waitForView(R.id.tvMail, 5000))
+        onView(withId(R.id.tvMail)).check(matches(isDisplayed()))
+        onView(withId(R.id.bLogout)).perform(click())
+        onView(withId(R.id.cbLink)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun loginTest() {
+        // set the TOKEN arguments editing the MainActivityTest Configuration
+        val args = InstrumentationRegistry.getArguments().getString("TOKEN")
+        onView(withId(R.id.tiPrivateCode)).perform(replaceText(args))
+        onView(withId(R.id.bInsertPrivate)).perform(click())
+        onView(isRoot()).perform(waitForView(R.id.tvMail, 5000))
+        onView(withId(R.id.tvMail)).check(matches(isDisplayed()))
+    }
+
+    fun navigateDownloadListTest() {
+        onView(withId(R.id.loadingCircle)).check(matches(isDisplayed()))
+        onView(isRoot()).perform(waitForView(R.id.tvMail, 5000))
+        onView(withId(R.id.navigation_lists)).perform(click())
+        onView(withId(R.id.tvMail)).check(doesNotExist())
     }
 }
