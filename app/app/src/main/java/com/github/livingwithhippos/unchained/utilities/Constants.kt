844ca926478f517757bc8f1370c62665490b0054
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2022-06-05 19:38:22 +0200

    fixed file path for release

--- app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/Constants.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/Constants.kt
@@ -9,7 +9,7 @@ const val BASE_AUTH_URL = "https://api.real-debrid.com/oauth/v2/"
 const val REFERRAL_LINK = "http://real-debrid.com/?id=78841"
 const val PREMIUM_LINK = "https://real-debrid.com/premium"
 const val ACCOUNT_LINK = "https://real-debrid.com/account"
-const val PLUGINS_PACK_LINK = "https://github.com/LivingWithHippos/unchained-android/raw/plugins-zip/extra_assets/plugins/unchained_plugins_pack.zip"
+const val PLUGINS_PACK_LINK = "https://github.com/LivingWithHippos/unchained-android/raw/master/extra_assets/plugins/unchained_plugins_pack.zip"
 const val PLUGINS_PACK_NAME = "unchained_plugins_pack.zip"
 const val PLUGINS_PACK_FOLDER = "pack"
 
