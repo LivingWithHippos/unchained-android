a073867d397a5bf8f899aafd547678d8320c2c1c
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2022-06-07 00:41:31 +0200

    Custom player (#222)
    * added web video cast as supported player
    
    close #199
    
    * added support for custom media player packages
    
    * updated settings layout

--- app/app/src/main/java/com/github/livingwithhippos/unchained/downloaddetails/view/DownloadDetailsFragment.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/downloaddetails/view/DownloadDetailsFragment.kt
@@ -322,6 +322,19 @@ class DownloadDetailsFragment : UnchainedFragment(), DownloadDetailsListener {
                     tryStartExternalApp(mxIntent)
                 }
             }
+            "web_video_cast" -> {
+                val wvcIntent = createMediaIntent("com.instantbits.cast.webvideo", url)
+                tryStartExternalApp(wvcIntent)
+            }
+            "custom_player" -> {
+                val customPlayerPackage = viewModel.getCustomPlayerPreference()
+                if (customPlayerPackage.isNullOrBlank()) {
+                    context?.showToast(R.string.invalid_package)
+                } else {
+                    val customIntent = createMediaIntent(customPlayerPackage, url)
+                    tryStartExternalApp(customIntent)
+                }
+            }
             else -> {
                 context?.showToast(R.string.missing_default_player)
             }
