ddb7832b2402dea384928e08ec051c56a702beae
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2022-07-04 00:10:52 +0200

    added adapters for checking torrent cache

--- app/app/src/main/java/com/github/livingwithhippos/unchained/data/remote/TorrentsApi.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/data/remote/TorrentsApi.kt
@@ -1,6 +1,8 @@
 package com.github.livingwithhippos.unchained.data.remote
 
 import com.github.livingwithhippos.unchained.data.model.AvailableHost
+import com.github.livingwithhippos.unchained.data.model.InstantAvailability
+import com.github.livingwithhippos.unchained.data.model.RdCache
 import com.github.livingwithhippos.unchained.data.model.TorrentItem
 import com.github.livingwithhippos.unchained.data.model.UploadedTorrent
 import okhttp3.RequestBody
@@ -15,6 +17,7 @@ import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
+import retrofit2.http.Url
 
 /**
  * This interface is used by Retrofit to manage all the REST calls to the torrents endpoints
@@ -89,4 +92,17 @@ interface TorrentsApi {
         @Header("Authorization") token: String,
         @Path("id") id: String,
     ): Response<Unit>
+
+
+    /**
+     * Check if any file of a torrent/magnet is already cached using their hashes
+     * Hashes must be passed like this:
+     *  https://api.real-debrid.com/rest/1.0/torrents/instantAvailability/HASH_1/HASH_2/HASH_3
+     *  etc. for any
+     */
+    @GET
+    suspend fun getInstantAvailability(
+        @Header("Authorization") token: String,
+        @Url url: String
+    ): Response<Map<String, RdCache>>
 }
