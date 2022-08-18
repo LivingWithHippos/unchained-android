ed62b65cf2935d7fe9f05a7cc3bbc9f838ca2df8
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2022-08-18 23:40:54 +0200

    added download management
    - manage stop action
    - better error messages

--- app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/download/DownloadWorker.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/utilities/download/DownloadWorker.kt
@@ -1,12 +1,15 @@
 package com.github.livingwithhippos.unchained.utilities.download
 
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.webkit.MimeTypeMap
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import androidx.documentfile.provider.DocumentFile
 import androidx.work.CoroutineWorker
+import androidx.work.ForegroundInfo
+import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import com.github.livingwithhippos.unchained.R
 import com.github.livingwithhippos.unchained.base.UnchainedApplication
@@ -21,33 +24,39 @@ import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import timber.log.Timber
 import java.net.URLConnection
+import java.util.*
 
 class DownloadWorker(appContext: Context, workerParams: WorkerParameters) :
     CoroutineWorker(appContext, workerParams) {
 
     private var job = Job()
     private val scope = CoroutineScope(Dispatchers.IO + job)
+    var shutdown = false
 
     override suspend fun doWork(): Result {
-        try {
-            val sourceUrl: String = inputData.getString(MainActivityViewModel.KEY_DOWNLOAD_SOURCE)!!
-            val folderUri: Uri =
-                Uri.parse(inputData.getString(MainActivityViewModel.KEY_FOLDER_URI)!!)
-            val fileName: String = inputData.getString(MainActivityViewModel.KEY_DOWNLOAD_NAME)!!
+        val sourceUrl: String = inputData.getString(MainActivityViewModel.KEY_DOWNLOAD_SOURCE) ?: return Result.failure()
+        val fileName: String = inputData.getString(MainActivityViewModel.KEY_DOWNLOAD_NAME) ?: return Result.failure()
+
+        val folderUri: Uri =
+            Uri.parse(inputData.getString(MainActivityViewModel.KEY_FOLDER_URI)!!)
+
+        val newFile: DocumentFile? = try {
+            getFileDocument(sourceUrl, folderUri, fileName)
+        } catch (ex: SecurityException) {
+            Timber.e("User has removed folder permissions")
+            null
+        }
+
+        if (newFile == null) {
+            Timber.e("Error getting download location file")
+            showToast(R.string.pick_download_folder)
+            return Result.failure()
+        }
+
+        val notificationID = newFile.hashCode()
 
         try {
-            val newFile: DocumentFile? = try {
-                getFileDocument(sourceUrl, folderUri, fileName)
-            } catch (ex: SecurityException) {
-                Timber.e("User has removed folder permissions")
-                null
-            }
 
-            if (newFile == null) {
-                Timber.e("Error getting download location file")
-                showToast(R.string.pick_download_folder)
-                return Result.failure()
-            }
 
             val outputStream = applicationContext.contentResolver.openOutputStream(newFile.uri)
             if (outputStream == null) {
@@ -56,9 +65,7 @@ class DownloadWorker(appContext: Context, workerParams: WorkerParameters) :
                 return Result.failure()
             }
 
-            val notificationID = newFile.hashCode()
-
-            // todo: customize this
+            // todo: use a single customized instance of this
             val client = OkHttpClient()
             val writer = FileWriter(
                 outputStream
@@ -70,63 +77,85 @@ class DownloadWorker(appContext: Context, workerParams: WorkerParameters) :
 
             var progressCounter = -1
             var lastNotificationTime = 0L
+
             scope.launch {
                 writer.state.collect {
-                    // todo: manage progress
+                    if (!isStopped) {
+                        when (it) {
+                            DownloadStatus.Completed -> {
+                                // this is managed below
+                            }
+                            is DownloadStatus.Error -> {
+                                setForeground(
+                                    makeStatusNotification(
+                                        id,
+                                        notificationID,
+                                        fileName,
+                                        applicationContext.getString(R.string.error),
+                                        applicationContext,
+                                        false
+                                    )
+                                )
 
-                    when (it) {
-                        DownloadStatus.Completed -> {
-                            // this is managed below
-                        }
-                        is DownloadStatus.Error -> {
-                            makeStatusNotification(
-                                notificationID,
-                                fileName,
-                                applicationContext.getString(R.string.error),
-                                applicationContext
-                            )
-                        }
-                        DownloadStatus.Paused -> {
-                            // todo: add this, it also needs to be onGoing
-                            makeStatusNotification(
-                                notificationID,
-                                fileName,
-                                applicationContext.getString(R.string.paused),
-                                applicationContext
-                            )
-                        }
-                        DownloadStatus.Queued -> {
-                            makeStatusNotification(
-                                notificationID,
-                                fileName,
-                                applicationContext.getString(R.string.queued),
-                                applicationContext
-                            )
-                        }
-                        DownloadStatus.Stopped -> {
-                            makeStatusNotification(
-                                notificationID,
-                                fileName,
-                                applicationContext.getString(R.string.stopped),
-                                applicationContext
-                            )
-                        }
-                        is DownloadStatus.Running -> {
-                            if (it.percent < 100 && it.percent != progressCounter && System.currentTimeMillis() - lastNotificationTime > 500) {
-                                lastNotificationTime = System.currentTimeMillis()
-                                Timber.e("DownloadStatus.Running progressCounter $progressCounter")
-                                progressCounter = it.percent
-                                makeProgressStatusNotification(
-                                    notificationID,
-                                    fileName,
-                                    applicationContext.getString(
-                                        R.string.download_in_progress_format,
-                                        it.percent
-                                    ),
-                                    it.percent,
-                                    applicationContext
+                            }
+                            DownloadStatus.Paused -> {
+                                setForeground(
+                                    makeStatusNotification(
+                                        id,
+                                        notificationID,
+                                        fileName,
+                                        applicationContext.getString(R.string.paused),
+                                        applicationContext
+                                    )
                                 )
                             }
+                            DownloadStatus.Queued -> {
+                                setForeground(
+                                    makeStatusNotification(
+                                        id,
+                                        notificationID,
+                                        fileName,
+                                        applicationContext.getString(R.string.queued),
+                                        applicationContext
+                                    )
+                                )
+                            }
+                            DownloadStatus.Stopped -> {
+                                setForeground(
+                                    makeStatusNotification(
+                                        id,
+                                        notificationID,
+                                        fileName,
+                                        applicationContext.getString(R.string.stopped),
+                                        applicationContext,
+                                        false
+                                    )
+                                )
+                            }
+                            is DownloadStatus.Running -> {
+                                if (it.percent < 100 && it.percent != progressCounter && System.currentTimeMillis() - lastNotificationTime > 500) {
+                                    lastNotificationTime = System.currentTimeMillis()
+                                    progressCounter = it.percent
+
+
+                                    setForeground(
+                                        makeProgressStatusNotification(
+                                            id,
+                                            notificationID,
+                                            fileName,
+                                            it.percent,
+                                            applicationContext
+                                        )
+                                    )
+
+                                }
+                            }
+                        }
+                    } else {
+                        // stops the download only the first time I get a notification
+                        if (!shutdown) {
+                            shutdown = true
+                            downloader.stop()
                         }
                     }
                 }
@@ -138,18 +167,35 @@ class DownloadWorker(appContext: Context, workerParams: WorkerParameters) :
 
             // todo: get whole size and check if it correspond
             return if (downloadedSize > 0) {
-                makeStatusNotification(
-                    notificationID,
-                    fileName,
-                    applicationContext.getString(R.string.download_complete),
-                    applicationContext
+                setForeground(
+                    makeStatusNotification(
+                        id,
+                        notificationID,
+                        fileName,
+                        applicationContext.getString(R.string.download_complete),
+                        applicationContext,
+                        false
+                    )
                 )
                 applicationContext.vibrate()
                 Result.success()
             } else
                 Result.failure()
+        } catch (e: android.accounts.NetworkErrorException) {
+            e.printStackTrace()
+
+            showToast(R.string.download_link_expired)
+            Timber.e("Exception occurred while downloading, ${e.message}")
+            return Result.failure()
         } catch (e: java.lang.Exception) {
             e.printStackTrace()
+
+            showToast(
+                applicationContext.getString(
+                    R.string.download_not_started_format,
+                    fileName
+                )
+            )
             Timber.e("Exception occurred while downloading, ${e.message}")
             return Result.failure()
         }
@@ -157,6 +203,10 @@ class DownloadWorker(appContext: Context, workerParams: WorkerParameters) :
 
     private suspend fun showToast(stringId: Int) = withContext(Dispatchers.Main) {
         applicationContext.showToast(stringId)
+    }
+
+    private suspend fun showToast(message: String) = withContext(Dispatchers.Main) {
+        applicationContext.showToast(message)
     }
 
     private fun getFileDocument(
@@ -194,15 +244,22 @@ class DownloadWorker(appContext: Context, workerParams: WorkerParameters) :
 
 const val GROUP_KEY_DOWNLOADS: String = "com.github.livingwithhippos.unchained.DOWNLOADS"
 
+
+// see https://developer.android.com/topic/libraries/architecture/workmanager/advanced/long-running
 fun makeStatusNotification(
+    workerId: UUID,
     id: Int,
     filename: String,
     title: String,
-    context: Context
-) {
+    context: Context,
+    onGoing: Boolean = true
+): ForegroundInfo {
 
+    // This PendingIntent can be used to cancel the worker
+    val stopIntent = WorkManager.getInstance(context)
+        .createCancelPendingIntent(workerId)
     // Create the notification
-    val builder = NotificationCompat.Builder(context, UnchainedApplication.DOWNLOAD_CHANNEL_ID)
+    val notification = NotificationCompat.Builder(context, UnchainedApplication.DOWNLOAD_CHANNEL_ID)
         .setSmallIcon(R.drawable.logo_no_background)
         .setCategory(NotificationCompat.CATEGORY_PROGRESS)
         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
@@ -210,24 +267,32 @@ fun makeStatusNotification(
         // setting setGroupSummary(false) will prevent this from showing up after the makeProgressStatusNotification one
         .setGroupSummary(true)
         .setProgress(0, 0, false)
-        .setOngoing(false)
+        .setOngoing(onGoing)
         .setContentTitle(title)
+        // todo:check if .setContentText(progress) is the same
         .setStyle(NotificationCompat.BigTextStyle().bigText(filename))
+        .addAction(R.drawable.icon_stop, context.getString(R.string.stop), stopIntent)
+        .build()
 
-    Timber.e("makeStatusNotification $title")
-    // Show the notification
-    NotificationManagerCompat.from(context).notify(id, builder.build())
+    return ForegroundInfo(id, notification)
 }
 
 fun makeProgressStatusNotification(
+    workerId: UUID,
     id: Int,
     filename: String,
-    title: String,
     progress: Int,
     context: Context
-) {
+): ForegroundInfo {
+    val title = context.getString(
+        R.string.download_in_progress_format,
+        progress
+    )
+    // This PendingIntent can be used to cancel the worker
+    val stopIntent = WorkManager.getInstance(context)
+        .createCancelPendingIntent(workerId)
     // Create the notification
-    val builder = NotificationCompat.Builder(context, UnchainedApplication.DOWNLOAD_CHANNEL_ID)
+    val notification = NotificationCompat.Builder(context, UnchainedApplication.DOWNLOAD_CHANNEL_ID)
         .setSmallIcon(R.drawable.logo_no_background)
         .setCategory(NotificationCompat.CATEGORY_PROGRESS)
         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
@@ -236,10 +301,12 @@ fun makeProgressStatusNotification(
         .setOngoing(true)
         .setProgress(100, progress, false)
         .setContentTitle(title)
+        // todo:check if .setContentText(progress) is the same
         .setStyle(NotificationCompat.BigTextStyle().bigText(filename))
+        .addAction(R.drawable.icon_stop, context.getString(R.string.stop), stopIntent)
+        .build()
 
-    // Show the notification
-    NotificationManagerCompat.from(context).notify(id, builder.build())
+    return ForegroundInfo(id, notification)
 }
 
 sealed class DownloadStatus {
