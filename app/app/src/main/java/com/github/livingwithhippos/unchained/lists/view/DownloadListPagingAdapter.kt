b1c6940b0d700848aa9440b8df4bb97fea32d1a3
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2025-10-11 14:27:55 +0200

    migrated download list

--- app/app/src/main/java/com/github/livingwithhippos/unchained/lists/view/DownloadListPagingAdapter.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/lists/view/DownloadListPagingAdapter.kt
@@ -1,23 +1,42 @@
 package com.github.livingwithhippos.unchained.lists.view
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.selection.ItemKeyProvider
+import androidx.recyclerview.selection.SelectionTracker
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
 import com.github.livingwithhippos.unchained.R
 import com.github.livingwithhippos.unchained.data.model.DownloadItem
-import com.github.livingwithhippos.unchained.utilities.DataBindingPagingAdapter
-import com.github.livingwithhippos.unchained.utilities.DataBindingPagingTrackedAdapter
+import com.github.livingwithhippos.unchained.databinding.ItemListDownloadBinding
+import com.github.livingwithhippos.unchained.utilities.extension.getFileSizeString
 
-/** A [DataBindingPagingAdapter] subclass. Displays a list of [DownloadItem]. */
-class DownloadListPagingAdapter(listener: DownloadListListener) :
-    DataBindingPagingTrackedAdapter<DownloadItem, DownloadListListener>(DiffCallback(), listener) {
+class DownloadListPagingAdapter(private val listener: DownloadListListener) :
+    PagingDataAdapter<DownloadItem, DownloadViewHolder>(DiffCallback()) {
+
+    var tracker: SelectionTracker<DownloadItem>? = null
 
     class DiffCallback : DiffUtil.ItemCallback<DownloadItem>() {
         override fun areItemsTheSame(oldItem: DownloadItem, newItem: DownloadItem): Boolean =
             oldItem.id == newItem.id
 
-        override fun areContentsTheSame(oldItem: DownloadItem, newItem: DownloadItem): Boolean {
-            // content does not change on update
-            return true
+        // content does not change on update
+        override fun areContentsTheSame(oldItem: DownloadItem, newItem: DownloadItem): Boolean =
+            true
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DownloadViewHolder {
+        val binding =
+            ItemListDownloadBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DownloadViewHolder(binding, listener)
+    }
+
+    override fun onBindViewHolder(holder: DownloadViewHolder, position: Int) {
+        val item = getItem(position)
+        if (item != null) {
+            holder.bindCell(item, tracker?.isSelected(item) ?: false)
         }
     }
 
@@ -29,6 +48,25 @@ class DownloadListPagingAdapter(listener: DownloadListListener) :
     }
 
     fun getPosition(id: String) = snapshot().indexOfFirst { it?.id == id }
+}
+
+class DownloadViewHolder(
+    private val binding: ItemListDownloadBinding,
+    private val listener: DownloadListListener,
+) : RecyclerView.ViewHolder(binding.root) {
+
+    var mItem: DownloadItem? = null
+
+    fun bindCell(item: DownloadItem, selected: Boolean) {
+        mItem = item
+        binding.tvTitle.text =
+            if (item.streamable == 1) itemView.context.getString(R.string.streaming)
+            else itemView.context.getString(R.string.download)
+        binding.tvName.text = item.filename
+        binding.tvSize.text = getFileSizeString(itemView.context, item.fileSize)
+        binding.selectionIndicator.visibility = if (selected) View.VISIBLE else View.GONE
+        binding.cvDownload.setOnClickListener { listener.onClick(item) }
+    }
 }
 
 interface DownloadListListener {
