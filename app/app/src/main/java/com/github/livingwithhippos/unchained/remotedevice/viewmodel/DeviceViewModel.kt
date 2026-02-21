ae57a8cacc6716d7b0127d50538a64ca574bc620
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2026-02-21 17:43:04 +0100

    WIP added jackett to remote device

--- app/app/src/main/java/com/github/livingwithhippos/unchained/remotedevice/viewmodel/DeviceViewModel.kt
+++ app/app/src/main/java/com/github/livingwithhippos/unchained/remotedevice/viewmodel/DeviceViewModel.kt
@@ -5,17 +5,85 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.livingwithhippos.unchained.data.local.RemoteDevice
 import com.github.livingwithhippos.unchained.data.local.RemoteService
+import com.github.livingwithhippos.unchained.data.local.RemoteServiceType
 import com.github.livingwithhippos.unchained.data.repository.RemoteDeviceRepository
+import com.github.livingwithhippos.unchained.di.ClassicClient
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
 import timber.log.Timber
 
 @HiltViewModel
-class DeviceViewModel @Inject constructor(private val deviceRepository: RemoteDeviceRepository) :
-    ViewModel() {
+class DeviceViewModel
+@Inject
+constructor(
+    private val deviceRepository: RemoteDeviceRepository,
+    @param:ClassicClient private val client: OkHttpClient,
+) : ViewModel() {
 
     val deviceLiveData = MutableLiveData<DeviceEvent>()
+
+    fun testService(
+        type: RemoteServiceType,
+        address: String,
+        port: Int,
+        username: String?,
+        password: String?,
+        apiToken: String?,
+    ) {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                when (type) {
+                    is RemoteServiceType.JACKETT -> {
+                        val url: StringBuilder = StringBuilder()
+                        if (
+                            !address.startsWith("http://", ignoreCase = true) &&
+                                !address.startsWith("https://", ignoreCase = true)
+                        ) {
+                            if (port == 443) url.append("https://") else url.append("http://")
+                        }
+                        url.append(address)
+                        if (port != 80 && port != 443) url.append(":$port")
+                        url.append("/api/v2.0/indexers/all/results/torznab/api?t=caps")
+                        if (apiToken != null) url.append("&apikey=$apiToken")
+                        val request = okhttp3.Request.Builder().url(url.toString()).build()
+                        try {
+                            val response = client.newCall(request).execute()
+                            if (response.isSuccessful) {
+                                Timber.d(response.body.toString())
+                                deviceLiveData.postValue(DeviceEvent.ServiceWorking)
+                            } else {
+                                deviceLiveData.postValue(
+                                    DeviceEvent.ServiceNotWorking(ServiceErrorType.ResponseError)
+                                )
+                            }
+                        } catch (e: Exception) {
+                            Timber.e(e, "Error testing the service $url")
+                            deviceLiveData.postValue(
+                                DeviceEvent.ServiceNotWorking(ServiceErrorType.Generic)
+                            )
+                        }
+                    }
+
+                    is RemoteServiceType.KODI -> {
+                        // todo: implement or manage from caller
+                        deviceLiveData.postValue(
+                            DeviceEvent.ServiceNotWorking(ServiceErrorType.InvalidService)
+                        )
+                    }
+
+                    is RemoteServiceType.VLC -> {
+                        deviceLiveData.postValue(
+                            DeviceEvent.ServiceNotWorking(ServiceErrorType.InvalidService)
+                        )
+                    }
+                }
+            }
+        }
+    }
 
     fun fetchRemoteDevices() {
         viewModelScope.launch {
@@ -100,6 +168,10 @@ class DeviceViewModel @Inject constructor(private val deviceRepository: RemoteDe
 }
 
 sealed class DeviceEvent {
+    data object ServiceWorking : DeviceEvent()
+
+    data class ServiceNotWorking(val errorType: ServiceErrorType) : DeviceEvent()
+
     data object DeletedAll : DeviceEvent()
 
     data object DeletedDevice : DeviceEvent()
@@ -118,4 +190,12 @@ sealed class DeviceEvent {
     data class Service(val service: RemoteService) : DeviceEvent()
 
     data class DeletedService(val service: RemoteService) : DeviceEvent()
+}
+
+sealed class ServiceErrorType {
+    data object ResponseError : ServiceErrorType()
+
+    data object InvalidService : ServiceErrorType()
+
+    data object Generic : ServiceErrorType()
 }
