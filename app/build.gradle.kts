c235daaf302d16daf370b3eaaee12fd89900005c
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2026-04-21 18:36:48 +0200

    updated libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.room) apply false
-    id("com.github.ben-manes.versions") version "0.53.0"
+    id("com.github.ben-manes.versions") version "0.54.0"
 }
 
 buildscript {
